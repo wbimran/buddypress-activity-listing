8f90b14b1f85e538da0f625258e80b586653bd6d
wbimran <githubimran@gmail.com>
2024-09-16 23:04:47 +0530

    Update Code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 /**
  * Registers a new block provided a unique name and an object defining its behavior.
  *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
  */
 import { registerBlockType } from '@wordpress/blocks';
 
@@ -10,7 +9,6 @@ import { registerBlockType } from '@wordpress/blocks';
  * All files containing `style` keyword are bundled together. The code used
  * gets applied both to the front of your site and to the editor.
  *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
  */
 import './style.scss';
 
@@ -24,7 +22,6 @@ import metadata from './block.json';
 /**
  * Every block starts by registering a new block type definition.
  *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
  */
 registerBlockType( metadata.name, {
 	/**
